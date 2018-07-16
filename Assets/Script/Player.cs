e04598908ee31b5373ffca03e58b961db9870cae
kohei1030 <climate_1030_kk@yahoo.co.jp>
2018-07-17 05:16:01 +0900

    メイン機能

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 位置座標
+    private Vector3 position;
+    // スクリーン座標をワールド座標に変換した位置座標
+    private Vector3 screenToWorldPointPosition;
+    // Use this for initialization
+
+    private const float MAX_POS = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float positionY = gameObject.transform.position.y;
+        float positionZ = gameObject.transform.position.z;
+        // Vector3でマウス位置座標を取得する
+        position.x = Input.mousePosition.x;
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        // ワールド座標に変換されたマウス座標を代入
+        gameObject.transform.position = new Vector3(screenToWorldPointPosition.x, positionY, positionZ);
+
+        if (transform.position.x >= MAX_POS)
+        {
+            transform.position = new Vector3(MAX_POS, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= MAX_POS * -1)
+        {
+            transform.position = new Vector3(MAX_POS * -1, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Script/Shot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+
+    private Rigidbody _rigidbody;
+    [SerializeField]
+    float SPEED = 10;
+    [SerializeField]
+    GameObject player;
+
+    private bool isStart;
+
+    void Start()
+    {
+        isStart = false;
+    }
+
+    void Update()
+    {
+        if (!isStart)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isStart = true;
+                _rigidbody = this.gameObject.GetComponent<Rigidbody>();
+                _rigidbody.AddForce(new Vector3(SPEED, SPEED, 0));
+            }
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1, transform.position.z);
+        }
+    }
+
+	void OnCollisionEnter(Collision collision)
+	{
+        if(collision.gameObject.tag == "Enemy"){
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.Z))
+        {
+            _rigidbody.velocity = new Vector3(Mathf.Abs(_rigidbody.velocity.x) * -1, _rigidbody.velocity.y, 0);
+        }
+
+        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.C))
+        {
+            _rigidbody.velocity = new Vector3(Mathf.Abs(_rigidbody.velocity.x), _rigidbody.velocity.y, 0);
+        }
+	}
+}
